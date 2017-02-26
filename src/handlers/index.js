8e831fdb03c0a4dc41a018666b3706da30c2556a
vrde <agranzot@gmail.com>
2017-02-26 15:49:06 +0100

    Reload the extension when DEVELOPMENT is true

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -3,4 +3,8 @@ export function registerHandlers (hub) {
     require('./logger').register(hub);
     require('./visualFeedback').register(hub);
     // require('./visualDebug').register(hub);
+
+    if (config.DEVELOPMENT) {
+        require('./reloadExtension').register(hub);
+    }
 }
